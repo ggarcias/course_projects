1b04e245410f2159c2a441236f988dac82b9b3f3
Amaia <amaia.marcano@gmail.com>
2021-05-21 10:07:07 +0200

    fixed

--- 02_project_folder_files_named_after_integer/script_01_folders_files_integer.sh
+++ 02_project_folder_files_named_after_integer/script_01_folders_files_integer.sh
@@ -3,14 +3,14 @@
 # 1) Create a bash script that takes 1 integer as an input and returns "file_<integer>.txt"
 # in the folder "folder_<integer>"
 
-echo "running ${pwd}/script_01_folders_files_integer.sh...";
+echo "running $(pwd)/script_01_folders_files_integer.sh...";
 
 mkdir folder_${1};
-echo "the directory folder_${1} was sucessfully created at ${pwd}/folder_${1}";
+echo "the directory folder_${1} was sucessfully created at $(pwd)/folder_${1}";
 ls;
 
 touch  folder_${1}/file_${1}
-echo "file_${1} was successfully created at ${pwd}/folder_${1}/file_${1}";
+echo "file_${1} was successfully created at $(pwd)/folder_${1}/file_${1}";
 ls;
 
 echo "done.";
